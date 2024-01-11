1d240ad50be95958d0f04ab40e736e4c7d3a78a4
Sachin Londhe <lsachin@mollatech.com>
2024-01-11 12:01:00 +0530

    Update provider.controller.js

--- openid-connect/src/Provider/provider.controller.js
+++ openid-connect/src/Provider/provider.controller.js
@@ -21,58 +21,20 @@ const route = (app, provider) => {
     next();
   }
 
-  // app.get("/authenticate/:uid", setNoCache, async (req, res, next) => {
-  //   try {
-  //     console.log({req})
-    
-  //     const interaction = await provider.interactionDetails(req, res);
-  //     const { uid, prompt, params,session, } = interaction;
-      
-  //     const client = await provider.Client.find(params.client_id);
-  //     // console.log(req.body)
-  //     if(prompt.name ==="consent" ){
-  //       return res.render("interaction", {
-  //         client,
-  //         user:"aj",
-  //         uid,
-  //         details: prompt.details,
-  //         params,
-  //         title: "Authorize",      
-  //       });
-  //     }
-
-  //     else if(prompt.name ==="login") {
-  //       res.cookie("uid", req.params.uid);
-  //       res.cookie("_interaction", req.cookies["_interaction"]);
-  //       res.cookie("connect.sid", req.cookies["connect.sid"]);
-  //       res.cookie("_interaction.sig", req.cookies["_interaction.sig"]);
-  //       return res.redirect(`${client.requestUris[0]}?requestId=${uid}`);
-  //     }
-      
-
-  //     return undefined;
-  //   } catch (err) {
-  //     return next(err);
-  //   }
-  // });
-
-
-
-
-
-  app.get("/authenticate/:uid/:axiomUserId", setNoCache, async (req, res, next) => {
+  app.get("/authenticate/:uid", setNoCache, async (req, res, next) => {
     try {
-      
+      // console.log({req})
     
       const interaction = await provider.interactionDetails(req, res);
-      const { uid, prompt, params,session, axiomUserId} = interaction;
-      console.log({axiomUserId})
+      
+      const { uid, prompt, params,session,lastSubmission } = interaction;
+      
       const client = await provider.Client.find(params.client_id);
       // console.log(req.body)
       if(prompt.name ==="consent" ){
         return res.render("interaction", {
           client,
-          user:"aj",
+          user:lastSubmission?.login?.axiomUserId || '',          
           uid,
           details: prompt.details,
           params,
@@ -95,6 +57,10 @@ const route = (app, provider) => {
     }
   });
 
+
+
+
+
   app.post(
     "/authenticate/:uid/login",
     setNoCache,
@@ -115,23 +81,13 @@ const route = (app, provider) => {
         const result = {
           login: {
             accountId: account.accountId,
-          //  ...req.body
+            axiomUserId:"SaCHIn"
           },
         };
 
-        //console.log({result});
-        
         const redirectTo = await provider.interactionResult(req, res, result);
-        
-        
-        // const queryParams = new URLSearchParams(result.login).toString();
-        // //console.log({queryParams})
-        // const finalURL= `${redirectTo}?${queryParams}`
-        // console.log({finalURL});
-        // // return res.redirect(redirectTo);
-        // res.query=result.login;
-        // req.query=result.login;
-        return res.redirect(`${redirectTo}/${req.body.userid}`);
+                
+        return res.redirect(`${redirectTo}`);
       } catch (err) {
         console.log(err);
         next(err);
